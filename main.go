d9bd1e60694da56ad8bc9fb4265c013651282d3a
Vhoris <vhoris@gmail.com>
2022-06-15 21:58:59 +0300

    Finish code modify

--- main.go
+++ main.go
@@ -5,19 +5,27 @@ import (
 	"math"
 )
 
-const ApplePrice = 5.99
-const PearPrice = 7.
-const AllMоnеу = 32.
+const applePrice = 5.99
+const pearPrice = 7.
+const allMоnеу = 32.
 
 func main() {
-	SumPriceAP := ApplePrice*9 + PearPrice*8
-	fmt.Println("1. Скільки грошей треба витратити, щоб купити 9 яблук та 8 груш? - ", SumPriceAP, "грн.")
+	
+	applesToBuy := 9
+	pearsToBuy := 8
+	
+	sumPriceAP := applePrice*float64(applesToBuy) + pearPrice*float64(pearsToBuy)
+	fmt.Println("1. Скільки грошей треба витратити, щоб купити 9 яблук та 8 груш? - ", sumPriceAP, "грн.")
 
-	SumPriceP := (AllMоnеу - math.Mod(AllMоnеу, PearPrice)) / PearPrice
-	fmt.Println("2. Скільки груш ми можемо купити? - ", SumPriceP)
+	sumPriceP := (allMоnеу - math.Mod(allMоnеу, pearPrice)) / pearPrice
+	fmt.Println("2. Скільки груш ми можемо купити? - ", sumPriceP)
 
-	SumPriceA := (AllMоnеу - math.Mod(AllMоnеу, ApplePrice)) / ApplePrice
-	fmt.Println("3. Скільки яблук ми можемо купити? - ", SumPriceA)
+	sumPriceA := (allMоnеу - math.Mod(allMоnеу, applePrice)) / applePrice
+	fmt.Println("3. Скільки яблук ми можемо купити? - ", sumPriceA)
+	
+	applesToBuy = 2
+	pearsToBuy = 2
 
-	fmt.Println("4. Чи ми можемо купити 2 груші та 2 яблука? - ", 2*(ApplePrice+PearPrice) <= AllMоnеу)
+
+	fmt.Println("4. Чи ми можемо купити 2 груші та 2 яблука? - ", (float64(applesToBuy)*applePrice+float64(pearsToBuy)*pearPrice) <= allMоnеу)
 }
